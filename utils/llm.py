df524a361270ad7aaf5b2ac28954f65f4abb478e
Shloki Jha <shlokijha@MacBook-Air-786.local>
2026-03-04 18:45:04 +0530

    updated version

--- utils/llm.py
+++ utils/llm.py
@@ -1,5 +1,5 @@
 import os
-import ollama
+# import ollama
 from google import genai
 from dotenv import load_dotenv
 
@@ -7,14 +7,14 @@ load_dotenv()
 
 def generate_response(model_name: str, prompt: str):
 
-    if model_name == "llama":
-        response = ollama.chat(
-            model="llama3",
-            messages=[{"role": "user", "content": prompt}]
-        )
-        return response["message"]["content"]
+    # if model_name == "llama":
+    #     response = ollama.chat(
+    #         model="llama3",
+    #         messages=[{"role": "user", "content": prompt}]
+    #     )
+    #     return response["message"]["content"]
 
-    elif model_name == "gemini":
+    if model_name == "gemini":
         client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
         response = client.models.generate_content(
             model="models/gemini-2.5-flash",
